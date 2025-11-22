471591003783a302a55ad13c0e817a1b5419317e
Swastik Bose <swastikbose31@gmail.com>
2025-11-23 00:01:30 +0530

    currency_convert created

---
+++ currency_convert/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:currency_convert/currency_convert_material_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// Types of Widgets
+// 1. Stateful Widgets - State is mutable i.e. can be altered
+// 2. Stateless Widgets - State is immutable
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CurrencyConvertMaterialPage(),
+    );
+  }
+}
